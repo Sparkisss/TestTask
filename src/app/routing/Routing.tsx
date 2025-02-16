e870e9e9d20df5ee2967fefab8524823ea3ffc0b
Igor Siamykin <sparkisisss@gmail.com>
2025-02-16 19:05:24 +0300

    chore: add deploy config

--- src/app/routing/Routing.tsx
+++ src/app/routing/Routing.tsx
@@ -1,18 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import { MainPage, CharacterPage, FavorCharacterPage, NotFound } from "@/pages";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainPage />,
-    errorElement: <NotFound />,
-  },
-  {
-    path: "/character/:characterId",
-    element: <CharacterPage />,
-  },
-  {
-    path: "/favor",
-    element: <FavorCharacterPage />,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainPage />,
+      errorElement: <NotFound />,
+    },
+    {
+      path: "/character/:characterId",
+      element: <CharacterPage />,
+    },
+    {
+      path: "/favor",
+      element: <FavorCharacterPage />,
+    },
+  ],
+  { basename: "/TestTask" },
+);
